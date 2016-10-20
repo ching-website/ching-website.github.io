0418ed074fd6d1632c9bde917f967209d0ed147a
paarthmadan <madanhockey@hotmail.com>
2016-10-20 11:19:23 -0400

    wip(home): rounded cards, added navbar fade on scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
+    console.log($('.navbar').height());
+
+    if($(window).scrollTop() > $('.navbar').height()){
+        $('.navbar').addClass("scrolled");
+        $('.navbar-brand').addClass("scrolled");
+    }else{
+    	$('.navbar').removeClass("scrolled");
+        $('.navbar-brand').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
